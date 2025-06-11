0c3682cf9ada71674d255d11906953dea94a4353
abhijeet1592006 <abhijeet8800434205@gmail.com>
2025-06-11 22:55:48 +0530

    update

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 app.secret_key="hello"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
 db=SQLAlchemy(app)
 
 class Data(db.Model):
-    id=db.column(db.Integer,primarykey=True)
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(25),nullable=False)
 
-db=SQLAlchemy(app)
-with app.app_context:
-    app.create_all()
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def main():
