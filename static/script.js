686612d11d3899a756b52c12ab1bf2fac37d5adb
abhijeet1592006 <abhijeet8800434205@gmail.com>
2025-06-14 21:25:52 +0530

    update

--- static/script.js
+++ static/script.js
@@ -1,9 +1,30 @@
+let products=[
+
+    {
+        pid:1,
+        pname:"hi",
+        price:111,
+    }
+
+
+]
+   
+
+
 function scrolls(a){
     const section=document.getElementById(a);
     section.scrollIntoView({behavior:"instant"})
     
 }
 
-function view(){
-    
+function view(uid){
+    console.log(uid)
+    console.log(typeof(uid))
+
+    for (let i of products){
+        if (i['pid']==uid){
+            console.log(i['pname'],i['price'])
+        }
+    }
+
 }
